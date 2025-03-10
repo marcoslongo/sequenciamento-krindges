708850ebf9d9d63a28dd20b7ee54538142237eb9
Marcos Vinicius dos Santos Longo <marcoslongo@pecege.com>
2025-03-10 17:38:12 -0300

    feat: desenvolve tela de validação de entrada

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,27 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/scrollbar';
 import Content from "@/components/layout/home/Content";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const { user, loading } = useAuth();
 
   if (loading) return <p>Carregando...</p>;
-  if (!user) return <p>Você precisa estar logado.</p>;
+  if (!user) return (
+    <div className="w-full h-[100vh] bg-[#0d0131] flex items-center justify-center">
+      <div className="flex justify-center items-center gap-4 flex-col">
+        <Image
+          src={'/assets/images/header_logo_2024.png'}
+          alt="alt"
+          width={200}
+          height={50}
+          className="invert filter"
+        />
+        <p className="text-white text-2xl">Você precisa estar logado! <Link className="hover:underline" href="/login">Fazer login</Link></p>
+      </div>
+    </div>
+  );
 
   return (
     <Content />
