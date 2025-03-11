d5829270aee707a5106bc519652d781e6d7f10d7
Marcos Vinicius dos Santos Longo <marcoslongo@pecege.com>
2025-03-11 18:24:09 -0300

    feat: desenvolve modal de mais informações da OP

--- components/CardOp/index.tsx
+++ components/CardOp/index.tsx
@@ -7,6 +7,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "../ui/button";
 
 interface CardOpProps {
   marca: string;
@@ -62,6 +63,34 @@ export function CardOP({
           <p>Dentro do prazo</p>
         </div>
       </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="flex w-full justify-center mt-3">
+            <Button variant="outline">Ver mais informações</Button>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-screen-sm">
+          <DialogHeader>
+            <DialogTitle>O.P: {nr_op} - Referência: {nr_op}</DialogTitle>
+          </DialogHeader>
+          <div className="font-bold">
+            <p>O.P: {nr_op}</p>
+            <p>Ref: 605 119072</p>
+            <p className="text-sm">Marca: <span>{marca}</span></p>
+            <p>Coleção: {ds_colecao}</p>
+            <p>Qt: {qt_op}</p>
+            <p>Entrada: {formatDate(inicio_op)}</p>
+            <div className="flex items-center gap-1">
+              <div className="bg-red-600 w-[24px] h-[24px] block rounded-full"></div>
+              <p>Atrasada</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="bg-green-500 w-[24px] h-[24px] block rounded-full"></div>
+              <p>Dentro do prazo</p>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
