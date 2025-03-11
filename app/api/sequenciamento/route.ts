c95307acba5c27a72caca2043f59f64b9da40f75
Marcos Vinicius dos Santos Longo <marcoslongo@pecege.com>
2025-03-11 17:40:06 -0300

    feat: desenvolve filtros

--- app/api/sequenciamento/route.ts
+++ app/api/sequenciamento/route.ts
@@ -3,8 +3,20 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
+    const { searchParams } = new URL(request.url);
+
+    const locais = searchParams.get("locais")?.split(",") || [];
+    const divisoes = searchParams.get("divisoes")?.split(",") || [];
+
+    const where = {
+      AND: [
+        locais.length > 0 ? { cd_local: { in: locais.map(Number) } } : {},
+        divisoes.length > 0 ? { ds_divisao_produtiva: { in: divisoes } } : {},
+      ],
+    };
+
     const aggregatedData = await prisma.tbSequenciamentoTeste.groupBy({
       by: ["cd_local", "ds_local"],
       _count: {
@@ -13,6 +25,7 @@ export async function GET() {
       _sum: {
         qt_op: true,
       },
+      where,
       orderBy: {
         cd_local: "asc",
       },
@@ -20,9 +33,11 @@ export async function GET() {
 
     const aggregatedDataProdutive = await prisma.tbSequenciamentoTeste.groupBy({
       by: ["ds_divisao_produtiva"],
+      where,
     });
 
     const detailedData = await prisma.tbSequenciamentoTeste.findMany({
+      where,
       orderBy: {
         cd_local: "asc",
       },
@@ -35,7 +50,11 @@ export async function GET() {
       pecas_local: item._sum.qt_op,
     }));
 
-    return NextResponse.json({ aggregatedData: processedAggregatedData, detailedData, aggregatedDataProdutive });
+    return NextResponse.json({
+      aggregatedData: processedAggregatedData,
+      detailedData,
+      aggregatedDataProdutive,
+    });
   } catch (error) {
     console.error("Erro ao buscar os dados:", error);
     return NextResponse.json({ error: "Erro ao buscar os dados" }, { status: 500 });
--- components/layout/home/Content/index.tsx
+++ components/layout/home/Content/index.tsx
@@ -1,22 +1,23 @@
-'use client'
-import { Checkbox } from "@/components/ui/checkbox";
+'use client';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/scrollbar';
 import { FreeMode, Scrollbar, Mousewheel } from 'swiper/modules';
-import { useState, useEffect } from "react";
-import { CardOP } from "@/components/CardOp";
-import { Header } from "../../Header";
+import { useState, useEffect } from 'react';
+import { CardOP } from '@/components/CardOp';
+import { Header } from '../../Header';
 import { Filter, Search } from 'lucide-react';
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from '@/components/ui/dialog';
 
 interface Sequenciamento {
   cd_local: number;
@@ -40,29 +41,70 @@ interface AggregatedData {
   ordens_local: number;
   pecas_local: number;
 }
+
 interface AggregatedDataProdutive {
   ds_divisao_produtiva: string;
 }
 
 export default function Content() {
-  const [dados, setDados] = useState<Sequenciamento[]>([]);
+  const [, setDados] = useState<Sequenciamento[]>([]); // Todos os dados
+  const [dadosFiltrados, setDadosFiltrados] = useState<Sequenciamento[]>([]);
   const [aggregatedData, setAggregatedData] = useState<AggregatedData[]>([]);
-  const [aggregatedDataProdutive, setAggregatedDataProdutive] = useState<AggregatedDataProdutive[]>([]);
+  const [aggregatedDataProdutive, setAggregatedDataProdutive] = useState<
+    AggregatedDataProdutive[]
+  >([]);
+  const [selectedLocais, setSelectedLocais] = useState<number[]>([]);
+  const [selectedDivisoes, setSelectedDivisoes] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch("/api/sequenciamento");
-        const resultado = await response.json();
-        setDados(resultado.detailedData);
-        setAggregatedData(resultado.aggregatedData);
-        setAggregatedDataProdutive(resultado.aggregatedDataProdutive);
-      } catch (error) {
-        console.error("Erro ao buscar os dados:", error);
-      }
-    }
     fetchData();
   }, []);
+  const fetchData = async (comFiltros: boolean = false) => {
+    setLoading(true);
+    try {
+      const url = new URL('/api/sequenciamento', window.location.origin);
+      if (comFiltros) {
+        if (selectedLocais.length > 0) {
+          url.searchParams.set('locais', selectedLocais.join(','));
+        }
+        if (selectedDivisoes.length > 0) {
+          url.searchParams.set('divisoes', selectedDivisoes.join(','));
+        }
+      }
+      const response = await fetch(url.toString());
+      const resultado = await response.json();
+
+      if (comFiltros) {
+        setDadosFiltrados(resultado.detailedData);
+      } else {
+        setDados(resultado.detailedData);
+        setDadosFiltrados(resultado.detailedData);
+        setAggregatedData(resultado.aggregatedData);
+        setAggregatedDataProdutive(resultado.aggregatedDataProdutive);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar os dados:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLocalChange = (cd_local: number) => {
+    setSelectedLocais((prev) =>
+      prev.includes(cd_local)
+        ? prev.filter((id) => id !== cd_local)
+        : [...prev, cd_local]
+    );
+  };
+
+  const handleDivisaoChange = (ds_divisao_produtiva: string) => {
+    setSelectedDivisoes((prev) =>
+      prev.includes(ds_divisao_produtiva)
+        ? prev.filter((divisao) => divisao !== ds_divisao_produtiva)
+        : [...prev, ds_divisao_produtiva]
+    );
+  };
 
   return (
     <main className="h-screen bg-gray-300">
@@ -70,7 +112,9 @@ export default function Content() {
       <div className="bg-[#0d0131] p-6 flex flex-col items-center gap-4">
         <Dialog>
           <DialogTrigger asChild>
-            <Button className="flex gap-1" variant="outline">Filtros <Filter /></Button>
+            <Button className="flex gap-1" variant="outline">
+              Filtros <Filter />
+            </Button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-md">
             <DialogHeader>
@@ -82,7 +126,12 @@ export default function Content() {
                   const checkboxId = `checkbox-${item.cd_local}`;
                   return (
                     <li key={index} className="flex items-center gap-1">
-                      <Checkbox value={item.cd_local} id={checkboxId} />
+                      <Checkbox
+                        value={item.cd_local}
+                        id={checkboxId}
+                        checked={selectedLocais.includes(item.cd_local)}
+                        onCheckedChange={() => handleLocalChange(item.cd_local)}
+                      />
                       <label htmlFor={checkboxId} className="cursor-pointer">
                         {item.cd_local} - {item.ds_local}
                       </label>
@@ -98,7 +147,16 @@ export default function Content() {
                   const checkboxId = `checkbox-${item.ds_divisao_produtiva}`;
                   return (
                     <li key={index} className="flex items-center gap-1">
-                      <Checkbox value={item.ds_divisao_produtiva} id={checkboxId} />
+                      <Checkbox
+                        value={item.ds_divisao_produtiva}
+                        id={checkboxId}
+                        checked={selectedDivisoes.includes(
+                          item.ds_divisao_produtiva
+                        )}
+                        onCheckedChange={() =>
+                          handleDivisaoChange(item.ds_divisao_produtiva)
+                        }
+                      />
                       <label htmlFor={checkboxId} className="cursor-pointer">
                         {item.ds_divisao_produtiva}
                       </label>
@@ -107,46 +165,71 @@ export default function Content() {
                 })}
               </ul>
               <div className="flex justify-center">
-                <Button className="flex gap-1 bg-[#0d0131]">Buscar<Search color="#fff" /></Button>
+                <DialogClose asChild>
+                  <Button
+                    className="flex gap-1 bg-[#0d0131]"
+                    onClick={() => fetchData(true)}
+                  >
+                    Buscar <Search color="#fff" />
+                  </Button>
+                </DialogClose>
               </div>
             </div>
           </DialogContent>
         </Dialog>
       </div>
       <div className="p-6">
-        <Swiper
-          direction={'horizontal'}
-          slidesPerView={5}
-          freeMode={true}
-          scrollbar={{ draggable: true }}
-          mousewheel={{ forceToAxis: true }}
-          modules={[FreeMode, Scrollbar, Mousewheel]}
-          className="mySwiper w-full"
-          spaceBetween={20}
-        >
-          {aggregatedData.map((item, index) => (
-            <SwiperSlide key={index} className="!w-[18%]">
-              <div className="text-center bg-white rounded py-4">
-                <h2 className="text-lg font-bold">{item.cd_local} - {item.ds_local}</h2>
-                <p className="text-md font-bold">Peças no Local: {item.pecas_local}</p>
-                <p className="text-md font-bold">Ordens no Local: {item.ordens_local}</p>
-              </div>
-              <div className="bg-white p-3 shadow rounded-md !h-[55vh] overflow-x-scroll overflow-y-auto">
-                {dados.filter((data) => data.cd_local === item.cd_local).map((item, index) => (
-                  <CardOP
-                    key={index}
-                    marca={item.ds_marca}
-                    desenho_tecnico={item.desenho_tecnico}
-                    nr_op={item.nr_op}
-                    ds_colecao={item.ds_colecao}
-                    qt_op={item.qt_op}
-                    inicio_op={item.inicio_op}
-                  />
-                ))}
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <p>Carregando...</p>
+          </div>
+        ) : (
+          <Swiper
+            direction={'horizontal'}
+            slidesPerView={5}
+            freeMode={true}
+            scrollbar={{ draggable: true }}
+            mousewheel={{ forceToAxis: true }}
+            modules={[FreeMode, Scrollbar, Mousewheel]}
+            className="mySwiper w-full"
+            spaceBetween={20}
+          >
+            {aggregatedData
+              .filter((item) => {
+                return dadosFiltrados.some((data) => data.cd_local === item.cd_local);
+              })
+              .map((item, index) => (
+                <SwiperSlide key={index} className="!w-[18%]">
+                  <div className="text-center bg-white rounded-t-md py-4">
+                    <h2 className="text-lg font-bold">
+                      {item.cd_local} - {item.ds_local}
+                    </h2>
+                    <p className="text-md font-bold">
+                      Peças no Local: {item.pecas_local}
+                    </p>
+                    <p className="text-md font-bold">
+                      Ordens no Local: {item.ordens_local}
+                    </p>
+                  </div>
+                  <div className="bg-white p-3 shadow rounded-b-md !h-[55vh] overflow-x-scroll overflow-y-auto">
+                    {dadosFiltrados
+                      .filter((data) => data.cd_local === item.cd_local)
+                      .map((item, index) => (
+                        <CardOP
+                          key={index}
+                          marca={item.ds_marca}
+                          desenho_tecnico={item.desenho_tecnico}
+                          nr_op={item.nr_op}
+                          ds_colecao={item.ds_colecao}
+                          qt_op={item.qt_op}
+                          inicio_op={item.inicio_op}
+                        />
+                      ))}
+                  </div>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        )}
       </div>
     </main>
   );
